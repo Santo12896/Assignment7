fd0fb9d6c93da27ba0bfdec73d4d5aeb71ed3ff7
santo12896 <santoislam12896@gmail.com>
2023-12-26 21:14:05 +0600

    bookmark cestructuring

---
+++ src/component/Bookmark/Bookmark.jsx
@@ -0,0 +1,15 @@
+const Bookmark = ({bookmark}) => {
+    const {course_name,price,credit} = bookmark;
+
+    return (
+        <div>
+            <h1>{course_name}</h1>
+            
+        </div>
+    );
+};
+
+
+
+export default Bookmark;
+
